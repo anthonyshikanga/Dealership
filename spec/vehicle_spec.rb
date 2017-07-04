c09f09dc384f936dbb1121aa046881d94f8fd104
anthonyshikanga <anthonyshikanaga@gmail.com>
2017-07-04 16:49:52 +0300

    modified vehicle_spec.rb file

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -49,6 +49,14 @@ require('rspec')
       end
     end
 
+    describe("#id") do
+      it("returns the id of the vehicle") do
+        test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
+        test_vehicle.save()
+        expect(test_vehicle.id()).to(eq(1))
+      end
+    end
+
     describe("#age") do
       it("returns the vehicles age") do
         test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
